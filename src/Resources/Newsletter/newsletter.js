7b878e737a5b1b9f0b1dbd737becb98afbedd56f
Gargicoder-03 <gargisharma187@gmail.com>
2026-08-07 21:51:08 +0530

    added july newsletter

--- src/Resources/Newsletter/newsletter.js
+++ src/Resources/Newsletter/newsletter.js
@@ -8,9 +8,18 @@ import new3 from '../Images/Newsletter/new3.png';
 import new4 from '../Images/Newsletter/new4.png';
 import new5 from '../Images/Newsletter/new5.png';
 import new6 from '../Images/Newsletter/jan.png';
+import new7 from '../Images/Newsletter/new7.png';
+
 
 
 const sessions = [
+  {
+    id: 8,
+    title: "July Edition",
+    image: new7,
+    link:'/Newsletter7.pdf'
+
+  },
   {
     id: 7,
     title: "January Edition",
